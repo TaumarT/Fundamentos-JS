80d312bdf4a00a3ba43b29b839b09b662dcfcc29
TaumarT <taiantaumar@gmail.com>
2022-07-27 20:04:20 -0300

    Add files via upload

---
+++ primeira_aula.js
@@ -0,0 +1,12 @@
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMat = primeiroNumero +segundoNumero;
+
+console.log(operacaoMat)
+
+const texto = "ola mundo ";
+const texto2 = "Taian Taumar";
+
+console.log(texto + texto2)
